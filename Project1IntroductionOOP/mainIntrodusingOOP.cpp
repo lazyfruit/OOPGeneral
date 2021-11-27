e3df701559701b444cc2b889faf27865142c791a
Ivan_ov65@itstepru.onmicrosoft.com <Ivan_ov65@itstepru.onmicrosoft.com>
2021-11-27 18:29:21 +0400

    reTeaching get/set methods

--- Project1IntroductionOOP/mainIntrodusingOOP.cpp
+++ Project1IntroductionOOP/mainIntrodusingOOP.cpp
@@ -1,21 +1,47 @@
 #include<iostream>
 using namespace std;
 
-class Point
+/*struct*/ class Point
 {
 	double x;
 	double y;
 public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
 };
+
+//#define STRUCT_POINT
 
 void main()
 {
 	setlocale(LC_ALL, "");
-
-	Point A;
+#ifdef STRUCT_POINT
+	//int a;		//Объявление переменной 'a' типа 'int'
+	Point A;		//Объявление переменной 'A' типа 'Point'
+					//Объявление (создание) экземпляра (объекта) 'A' структуры 'Point'
 	A.x = 2; A.y = 3;
 	cout << A.x << "\t" << A.y << endl;
 
 	Point* pA = &A;
 	cout << pA->x << "\t" << pA->y << endl;
+#endif // STRUCT_POINT
+
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
 }
