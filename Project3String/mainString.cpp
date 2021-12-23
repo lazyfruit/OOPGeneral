b0b35a12270838fc9a66c1453ff3738d5e128949
Ivan_ov65@itstepru.onmicrosoft.com <Ivan_ov65@itstepru.onmicrosoft.com>
2021-12-23 17:19:40 +0400

    Reteaching commit

--- Project3String/mainString.cpp
+++ Project3String/mainString.cpp
@@ -13,7 +13,7 @@ String operator+(const String& left, const String& right);
 class String
 {
 	int size;	//Размер строки в байтах
-	char* str;	//Àäðåñ ñòðîêè â äèíàìè÷åñêîé ïàìÿòè
+	char* str;	//Адрес строки в динамической памяти
 public:
 	int get_size()const
 	{
@@ -27,8 +27,7 @@ public:
 	{
 		return str;
 	}
-
-	//			Constructors:
+	//		Constructors:
 	String(int size = 80)
 	{
 		this->size = size;
@@ -42,20 +41,16 @@ public:
 		for (int i = 0; i < size; i++)this->str[i] = str[i];
 		cout << "Constructor:\t\t" << this << endl;
 	}
-	//			The rule of three
 	String(const String& other)
 	{
-		//Deep copy
 		this->size = other.size;
 		this->str = new char[size] {};
 		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
 		cout << "CopyConstructor:\t" << this << endl;
 	}
 	String(String&& other)
-	{
-		//Shallow copy - Ïîâåðõíîñòîå êîïèðîâàíèå
-		//Êîíñòðóêòîð ïåðåíîñà äîëæåí ðàáîòàòü òàê,
-		//êàê ÍÅ äîëæåí ðàáîòàòü êîíñòðóêòîð êîïèîâàíèÿ.
+	{//Shallow copy - поверхностное копирование
+	 //Конструктор переноса должен работать так, как НЕ должен работать коструктор копирования
 		this->size = other.size;
 		this->str = other.str;
 		cout << "MoveConstructor:\t" << this << endl;
@@ -66,21 +61,17 @@ public:
 	{
 		delete[] this->str;
 		cout << "Destructor:\t\t" << this << endl;
-		//Memory leak
 	}
-
-	//				Operators
+	//		Operators
 	String& operator=(const String& other)
 	{
 		if (this == &other)return *this;
 		delete[] this->str;
-		//Deep copy:
+		//Deep copy (глубокое копирование):
 		this->size = other.size;
 		this->str = new char[size] {};
 		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
 		cout << "CopyAssigment:\t\t" << this << endl;
-		//Assignment - Ïðèñâàèâàíèå
-		//Assign - Ïðèñâîèòü
 		return *this;
 	}
 	String& operator=(String&& other)
@@ -95,7 +86,6 @@ public:
 	{
 		return *this = *this + other;
 	}
-
 	const char& operator[](int i)const
 	{
 		return str[i];
@@ -104,28 +94,25 @@ public:
 	{
 		return str[i];
 	}
-
 	void print()const
 	{
 		cout << "Size:\t" << size << endl;
 		cout << "Str:\t" << str << endl;
 	}
 };
-
 String operator+(const String& left, const String& right)
 {
 	String buffer = left.get_size() + right.get_size() - 1;
 	for (int i = 0; i < left.get_size(); i++)
 		//buffer.get_str()[i] = left.get_str()[i];
 		buffer[i] = left[i];
-	//				l-value = r-value;
+	//	  l-value = r-value;
 	for (int i = 0; i < right.get_size(); i++)
 		//buffer.get_str()[i + left.get_size() - 1] = right.get_str()[i];
 		buffer[i + left.get_size() - 1] = right[i];
 
 	return buffer;
 }
-
 ostream& operator<<(ostream& os, const String& obj)
 {
 	return os << obj.get_str();
@@ -149,7 +136,7 @@ void main()
 	cout << str2 << endl;
 
 	String str3;
-	str3 = str2;	//Shallow copy - ïîâåðõíîñòíîå êîïèðîâàíèå
+	str3 = str2;	//Shallow copy - поверностное копирование
 	cout << str3 << endl;
 #endif // CONSTRUCTORS_CHECK
 
@@ -167,9 +154,4 @@ void main()
 	str1 += str2;
 	razdel;
 	cout << str1 << endl;*/
-
-	//Move-ìåòîäû (Move-semantic) Ñ++ 11
-	//Move-constructor (Êîíñòðóêòîð ïåðåíîñà)
-	//Move-assignment  (Îïåðàòîð ïðèñâàíèÿ ïåðåíîñà)
-	//reference to r-value: Class&&
 }
