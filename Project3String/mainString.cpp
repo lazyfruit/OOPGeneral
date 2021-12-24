84450c2b125966989b3ab2f1f7642483799d28cb
Ivan_ov65@itstepru.onmicrosoft.com <Ivan_ov65@itstepru.onmicrosoft.com>
2021-12-24 15:37:20 +0400

    Header initialisation

--- Project3String/mainString.cpp
+++ Project3String/mainString.cpp
@@ -28,34 +28,34 @@ public:
 		return str;
 	}
 	//		Constructors:
-	String(int size = 80)
+	explicit String(int size = 80):size(size)/*Инициализация в заголовке*/, str(new char[size]{})/*Выделение памяти в заголовке*/
 	{
-		this->size = size;
-		this->str = new char[size] {};
+		/*this->size = size;*/
+		/*this->str = new char[size] {};*/
 		cout << "DefaultConstructor:\t" << this << endl;
 	}
-	String(const char* str)
+	String(const char* str):size(strlen(str)+1), str(new char[size]{})
 	{
-		this->size = strlen(str) + 1;
-		this->str = new char[size] {};
+		/*this->size = strlen(str) + 1;*/
+		/*this->str = new char[size] {};*/
 		for (int i = 0; i < size; i++)this->str[i] = str[i];
 		cout << "Constructor:\t\t" << this << endl;
 	}
-	String(const String& other)
+	String(const String& other):size(other.size), str(new char[size] {})
 	{
-		this->size = other.size;
-		this->str = new char[size] {};
+		/*this->size = other.size;*/
+		/*this->str = new char[size] {};*/
 		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
 		cout << "CopyConstructor:\t" << this << endl;
 	}
-	String(String&& other)
+	String(String&& other):size(other.size), str(other.str)
 	{//Shallow copy - поверхностное копирование
 	 //Конструктор переноса должен работать так, как НЕ должен работать коструктор копирования
-		this->size = other.size;
-		this->str = other.str;
-		cout << "MoveConstructor:\t" << this << endl;
-		other.str = nullptr;
+		/*this->size = other.size;*/
+		/*this->str = other.str;*/
+				other.str = nullptr;
 		other.size = 0;
+		cout << "MoveConstructor:\t" << this << endl;
 	}
 	~String()
 	{
@@ -102,7 +102,7 @@ public:
 };
 String operator+(const String& left, const String& right)
 {
-	String buffer = left.get_size() + right.get_size() - 1;
+	String buffer (left.get_size() + right.get_size() - 1);
 	for (int i = 0; i < left.get_size(); i++)
 		//buffer.get_str()[i] = left.get_str()[i];
 		buffer[i] = left[i];
@@ -119,6 +119,7 @@ ostream& operator<<(ostream& os, const String& obj)
 }
 
 //#define CONSTRUCTORS_CHECK
+//#define OPERATOR_PLUS_CHECK
 
 void main()
 {
@@ -139,10 +140,10 @@ void main()
 	str3 = str2;	//Shallow copy - поверностное копирование
 	cout << str3 << endl;
 #endif // CONSTRUCTORS_CHECK
-
+#ifdef OPERATOR_PLUS_CHECK
 	/*String str1 = "Hello";
-	str1 = str1;
-	cout << str1 << endl;*/
+str1 = str1;
+cout << str1 << endl;*/
 
 	String str1 = "Hello";
 	String str2 = "World";
@@ -154,4 +155,29 @@ void main()
 	str1 += str2;
 	razdel;
 	cout << str1 << endl;*/
+#endif // OPERATOR_PLUS_CHECK
+
+	/*String str1;*/	//Default constructor
+	String str1(25);
+	/*cout << str1 << endl;*/
+	str1.print();
+	String str2 = "Hello";	//Single-argement constructor
+	/*cout << str2 << endl;*/
+	str2.print();
+	String str3("World");	//Single-argement constructor
+	/*cout << str3 << endl;*/
+	str3.print();
+	String str4();	//Объект не создается,\
+					а объявляется функция str4, которая ничего не принимает и возвращает объект класса String
+	/*cout << str4 << endl;*/	//Не будет работать!
+	String str5{};	//Явный вызов конструктора по умолчанию
+	/*cout << str5 << endl;*/
+	str5.print();
+	String str6{ "Параметры в конструктор можно передавать в фигурных скобках" };	//Single-argement constructor
+	/*cout << str6 << endl;*/
+	str6.print();
+	String str7 = str6;
+	/*cout << str6 << endl;*/
+	str7.print();
+	cout << str2 + " " + str3 << endl;
 }
