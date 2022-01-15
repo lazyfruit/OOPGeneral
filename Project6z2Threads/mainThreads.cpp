d7a3b240af7f53b9f2c32b35852c6b87c7339e28
Ivan_ov65@itstepru.onmicrosoft.com <Ivan_ov65@itstepru.onmicrosoft.com>
2022-01-15 16:30:19 +0400

    mainThreads reteaching

---
+++ Project6z2Threads/mainThreads.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<thread>
+#include<Windows.h>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+#define line "\n______________________________________________________________\n"
+#define sleep /*Sleep(10);*/ /*Sleep(100);*/ Sleep(1000);
+
+bool finish = false;
+void Hello()
+{
+	while (!finish)
+	{
+		//cout << "Hello";
+		cout << "-";
+		sleep
+	}
+}
+void World()
+{
+	while (!finish)
+	{
+		//cout << "World";
+		cout << "+";
+		sleep
+	}
+}
+void Goodbye()
+{
+	while (!finish)
+	{
+		//cout << "Goodbye";
+		cout << "=";
+		sleep
+	}
+}
+void Dream()
+{
+	while (!finish)
+	{
+		//cout << "Dream";
+		cout << "/";
+		sleep
+	}
+}
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	cout << "Для начала работы программы нажмите \"Enter\". Для завершения повторно нажмите \"Enter\"." << endl; cin.get();
+	std::thread hello_thread(Hello);
+	std::thread world_thread(World);
+	std::thread goodbye_thread(Goodbye);
+	std::thread dream_thread(Dream);
+
+	cin.get(); finish = true;
+
+	world_thread.join();
+	hello_thread.join();
+	goodbye_thread.join();
+	dream_thread.join();
+}
